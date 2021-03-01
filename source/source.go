86b471918dae212f4d5852e7ebf6db0b775ceafa
smallteeths <503630985@qq.com>
2021-03-01 13:21:36 +0800

    feat: Suport F5bigip provider

--- source/source.go
+++ source/source.go
@@ -137,6 +137,12 @@ func getProviderSpecificAnnotations(annotations map[string]string) (endpoint.Pro
 				Name:  fmt.Sprintf("aws/%s", attr),
 				Value: v,
 			})
+		} else if strings.HasPrefix(k, "external-dns.alpha.kubernetes.io/bigip-") {
+			attr := strings.TrimPrefix(k, "external-dns.alpha.kubernetes.io/bigip-")
+			providerSpecificAnnotations = append(providerSpecificAnnotations, endpoint.ProviderSpecificProperty{
+				Name:  fmt.Sprintf("bigip/%s", attr),
+				Value: v,
+			})
 		}
 	}
 	return providerSpecificAnnotations, setIdentifier
